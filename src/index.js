84b3547660a8b383280e9385539a765de626a019
TopMak <26412430+TopMak@users.noreply.github.com>
2018-05-18 11:47:36 +0300

    Teht1.3 - changed osa data to objects

--- src/index.js
+++ src/index.js
@@ -34,18 +34,24 @@ const Yhteensa = (props) => {
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const osa1 = {
+    nimi: 'Reactin perusteet',
+    tehtavia: 10
+  }
+  const osa2 = {
+    nimi: 'Tiedonvälitys propseilla',
+    tehtavia: 7
+  }
+  const osa3 = {
+    nimi: 'Komponenttien tila',
+    tehtavia: 14
+  }
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto sis1={osa1} sis2={osa2} sis3={osa3} teht1={tehtavia1} teht2={tehtavia2} teht3={tehtavia3}  />
-      <Yhteensa teht1={tehtavia1} teht2={tehtavia2} teht3={tehtavia3} />
+      <Sisalto sis1={osa1.nimi} sis2={osa2.nimi} sis3={osa3.nimi} teht1={osa1.tehtavia} teht2={osa2.tehtavia} teht3={osa3.tehtavia}  />
+      <Yhteensa teht1={osa1.tehtavia} teht2={osa2.tehtavia} teht3={osa3.tehtavia} />
     </div>
   )
 }
